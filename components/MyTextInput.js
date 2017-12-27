2e76df81652b1d18aa709df065473f3992ed07bf
Marc Edwards <mdouglasedwards@gmail.com>
2017-12-26 19:58:50 -0500

    Refactor: create text input component wrapper for redux forms

--- components/MyTextInput.js
+++ components/MyTextInput.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { TextInput, View, Text } from 'react-native';
-
+import { TextInput, View, Text, StyleSheet } from 'react-native';
+import { purple, white, errorColor, errorTextColor } from '../utils/colors';
 /**
  * to be wrapped with redux-form Field component
  */
 export default function MyTextInput(props) {
   const { input, meta: { touched, error, warning }, ...inputProps } = props;
+
+  const errorStyle = touched && error ? styles.inputError : {};
 
   return (
     <View>
@@ -15,7 +17,30 @@ export default function MyTextInput(props) {
         onBlur={input.onBlur}
         onFocus={input.onFocus}
         value={input.value}
+        style={[styles.input, errorStyle]}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 20,
+    backgroundColor: purple
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 40,
+    width: 300,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginBottom: 30,
+    padding: 10
+  },
+  inputError: {
+    backgroundColor: errorColor,
+    color: errorTextColor,
+    borderColor: errorTextColor
+  }
+});
