17f95913398a2bc8b29733f6eafd947e906709f8
Marc Edwards <mdouglasedwards@gmail.com>
2017-12-26 19:59:37 -0500

    Feature: add new colors to color helper

--- utils/colors.js
+++ utils/colors.js
@@ -9,3 +9,5 @@ export const blue = '#4e4cb8';
 export const lightPurp = '#7c53c3';
 export const pink = '#b93fb3';
 export const silver = '#7a7a7a';
+export const errorColor = '#f2dede';
+export const errorTextColor = '#a94442';
